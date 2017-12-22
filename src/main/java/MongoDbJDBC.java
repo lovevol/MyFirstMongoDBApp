34ff278756e9bf7ec371cb6eef153a5c79149e03
lh <935686942@qq.com>
2017-12-22 20:48:16 +0800

    插入数据

--- src/main/java/MongoDbJDBC.java
+++ src/main/java/MongoDbJDBC.java
@@ -5,20 +5,33 @@ import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.util.*;
+
 public class MongoDbJDBC {
     public static void main(String[] args) {
         try {
+            //连接客户端并获取集和
             MongoClient mongoClient = new MongoClient("localhost",27017);
             MongoDatabase mongoDatabase = mongoClient.getDatabase("mytest");
             MongoCollection<Document> collection = mongoDatabase.getCollection("users");
+            //查询所有文档
             FindIterable<Document> findIterable = collection.find();
             MongoCursor<Document> cursor = findIterable.iterator();
             while (cursor.hasNext()){
                 System.out.println(cursor.next());
             }
-            /*for (Document aFindIterable : findIterable) {
-                System.out.println(aFindIterable);
-            }*/
+            //插入文档
+            Document favorites = new Document("food", Arrays.asList('a','b','c','d'));//文档作为值
+            Document document = new Document("username","lh555").append("age",555).append("favorites",favorites);
+            List<Document> documents = new ArrayList<Document>();
+            documents.add(document);
+            collection.insertMany(documents);
+
+            findIterable = collection.find();
+            for (Document adocument:findIterable){
+                System.out.println(adocument);
+            }
+
         }catch (Exception e){
             e.printStackTrace();
         }
