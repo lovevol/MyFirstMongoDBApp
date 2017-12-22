c9fcdf52a12398944a606960d8a276f290d7de45
lh <935686942@qq.com>
2017-12-22 21:12:52 +0800

    CRUD

--- src/main/java/MongoDbJDBC.java
+++ src/main/java/MongoDbJDBC.java
@@ -3,6 +3,7 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 
 import java.util.*;
@@ -10,23 +11,32 @@ import java.util.*;
 public class MongoDbJDBC {
     public static void main(String[] args) {
         try {
-            //连接客户端并获取集和
+            //连接客户端并获取集合
             MongoClient mongoClient = new MongoClient("localhost",27017);
             MongoDatabase mongoDatabase = mongoClient.getDatabase("mytest");
             MongoCollection<Document> collection = mongoDatabase.getCollection("users");
+
             //查询所有文档
             FindIterable<Document> findIterable = collection.find();
             MongoCursor<Document> cursor = findIterable.iterator();
             while (cursor.hasNext()){
                 System.out.println(cursor.next());
             }
-            //插入文档
-            Document favorites = new Document("food", Arrays.asList('a','b','c','d'));//文档作为值
-            Document document = new Document("username","lh555").append("age",555).append("favorites",favorites);
-            List<Document> documents = new ArrayList<Document>();
-            documents.add(document);
-            collection.insertMany(documents);
 
+            //插入文档
+            //Document favorites = new Document("food", Arrays.asList('a','b','c','d')).append("other",Arrays.asList('1','2','3','4'));//文档作为值
+            //Document document = new Document("username","lh555").append("age",555).append("favorites",favorites);
+            //List<Document> documents = new ArrayList<Document>();
+            //documents.add(document);
+            //collection.insertMany(documents);
+
+
+
+            //更新文档
+            //collection.updateMany(Filters.eq("username","lh555"),new Document("$set",new Document("age","22")));
+            //删除文档
+            collection.deleteOne(Filters.eq("username","lh"));
+            collection.deleteMany(Filters.eq("username","lh555"));
             findIterable = collection.find();
             for (Document adocument:findIterable){
                 System.out.println(adocument);
